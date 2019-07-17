8195d8ec289fa76da465d9ef952544776ee3573d
corbecker <corbinbecker@protonmail.com>
2019-07-17 13:27:58 +0100

    geolocation setup

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SeasonDisplay from './SeasonDisplay';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//location user and current month
+//pass season to seasondisplay component
+
+const App = () => {
+
+
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    err => console.log(err)
+  );
+
+  return (
+    <div>
+      <SeasonDisplay />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
