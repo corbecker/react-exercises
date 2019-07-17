afc6bcf91afa6ed954c0bbae5788f4d9fa98687b
corbecker <corbinbecker@protonmail.com>
2019-07-17 14:01:23 +0100

    conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -11,22 +11,25 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ''
     }
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
-      err => console.log(err)
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <SeasonDisplay />
-      </div>
-    )
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading...</div>
   }
 }
 
