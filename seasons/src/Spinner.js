a8efc143f4836c97b20a14e2aaf61e398612af38
corbecker <corbinbecker@protonmail.com>
2019-07-18 06:30:57 +0100

    loading spinner

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui big loader"></div>
+    </div>
+  )
+}
+
+export default Spinner;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 //location user and current month
 //pass season to seasondisplay component
@@ -29,7 +30,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading...</div>
+    return <Spinner />
   }
 }
 
