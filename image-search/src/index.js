cbc83ecf217f58573e46fc6ce0d949c6220f6d9c
corbecker <corbinbecker@protonmail.com>
2019-07-20 05:33:01 +0100

    folder restructure

--- image-search/src/index.js
+++ image-search/src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './SearchBar';
-
-const App = () => {
-  return (
-    <SearchBar />
-  )
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
