dd8686d781362cf35547e572f73269997cb2dcea
corbecker <corbinbecker@protonmail.com>
2019-07-20 05:44:07 +0100

    semantic ui & state

--- image-search/src/components/SearchBar.js
+++ image-search/src/components/SearchBar.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <input type="text" name="search"></input>
-  )
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" name="search" value={this.state.term} onChange={this.handleChange}></input>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SearchBar;
