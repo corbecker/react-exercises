644e14faff0a166101ea7b60aef9ec83a4d4c0e5
corbecker <corbinbecker@protonmail.com>
2019-07-22 06:02:38 +0100

    default state

--- youtube-clone/src/components/App.js
+++ youtube-clone/src/components/App.js
@@ -11,11 +11,18 @@ class App extends React.Component {
     selectedVideo: null
   }
 
+  componentDidMount = () => {
+    this.onTermSubmit('ireland');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search/', {
       params: { q: term }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   }
 
   onVideoSelect = (video) => {
