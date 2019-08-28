64610b682013aa4cf06abdf20ed3555501a661a4
corbecker <corbinbecker@protonmail.com>
2019-08-28 11:42:28 +0100

    connect songlist

--- songs/songs/src/components/App.js
+++ songs/songs/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { selectSong } from '../actions';
+import SongList from './SongList';
 
 const App = () => {
   return (
-    <div>yas queen</div>
-  )
-}
+    <div>
+      <SongList />
+    </div>
+  );
+};
 
 export default App;
