9aee1664f6a8e4d6df0b283a15998f68110e4ff6
corbecker <corbinbecker@protonmail.com>
2019-08-29 11:50:00 +0100

    PostList component

---
+++ redux-blog-posts/src/components/PostList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PostList extends React.Component {
+  render() {
+    return <div>posts</div>
+  }
+}
+
+export default PostList;
--- redux-blog-posts/src/index.js
+++ redux-blog-posts/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-const App = () => {
-  retrun(
-    <div>Yas queen</div>
-  )
-}
+import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
 
