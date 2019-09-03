4cff527fe1c4ffcfac81b2d2d7f4395b5f608247
corbecker <corbinbecker@protonmail.com>
2019-09-03 10:14:28 +0100

    react router scaffolding

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamCreate from './streams/StreamCreate';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </BrowserRouter>
+    </div>
   )
 };
 
-export default App;
+export default App; 
