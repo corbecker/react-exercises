f94fb155367b354be25d3471e104a87b954fdb83
corbecker <corbinbecker@protonmail.com>
2019-09-04 07:42:08 +0100

    listen for authchange

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -16,8 +16,13 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedin: this.auth.isSignedIn.get() });
   }
 
   renderAuthButton() {
