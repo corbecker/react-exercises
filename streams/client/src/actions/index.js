9a11582c9e676f56c421f78555bb6849013cf2c2
corbecker <corbinbecker@protonmail.com>
2019-09-06 12:31:42 +0100

    redux form stream create inputs wired up

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -2,7 +2,6 @@ import { SIGN_IN, SIGN_OUT } from './types'
 
 // Sign In called after successful sign in
 export const signIn = (id) => {
-  console.log(id)
   return {
     type: SIGN_IN,
     payload: id
--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  )
+class StreamCreate extends React.Component {
+
+  renderInput({ input }) {
+
+    return (
+      //spread in all the props passed in from redux-form
+      <input {...input} />
+    );
+  };
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    )
+  };
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
