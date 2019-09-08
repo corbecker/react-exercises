cf554ff7731078032ad7231a6f7de89d8f84f673
corbecker <corbinbecker@protonmail.com>
2019-09-08 20:08:05 +0100

    REST api for streams action creators

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,4 @@
-import { SIGN_IN, SIGN_OUT } from './types'
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, GET_STREAMS, GET_STREAM, DELETE_STREAM, UPDATE_STREAM } from './types'
 import streams from '../apis/streams';
 
 // Sign In called after successful sign in
@@ -18,6 +18,36 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
+    //send off to reducer
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+  };
+};
+
+export const getStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/stream/${id}`);
+    dispatch({ type: GET_STREAM, payload: response.data });
+  };
+};
+
+export const getStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get('/streams');
+    dispatch({ type: GET_STREAMS, payload: response.data });
+  };
+};
+
+export const updateStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: UPDATE_STREAM, payload: response.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id })
   };
 };
