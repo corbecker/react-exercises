762a7394fec074ea69a6145edaf17a08f1178174
corbecker <corbinbecker@protonmail.com>
2019-09-17 14:54:34 +0100

    refactored to use context consumer

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,14 +2,15 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  // property must be named contextType to work
-  static contextType = LanguageContext;
 
   render() {
-    const name = this.context === 'english' ? 'Submit' : 'Voorleggen';
 
     return (
-      <button className="ui button primary" > {name}</button>
+      <button className="ui button primary" >
+        <LanguageContext.Consumer>
+          {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+        </LanguageContext.Consumer>
+      </button>
     )
   };
 };
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -2,13 +2,15 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
-  static contextType = LanguageContext;
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Naam';
     return (
       <div className="ui field">
-        <label className="ui">{text}</label>
+        <label className="ui">
+          <LanguageContext.Consumer>
+            {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+          </LanguageContext.Consumer>
+        </label>
         <input />
       </div>
     )
