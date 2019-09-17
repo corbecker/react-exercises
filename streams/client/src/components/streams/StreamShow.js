ff4b3b1ecf1a7babe89eb2fdb475310eff562559
corbecker <corbinbecker@protonmail.com>
2019-09-17 11:15:05 +0100

    conditional render for video player

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -15,7 +15,19 @@ class StreamShow extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getStream(id);
+    this.buildPlayer();
+  }
 
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.props.getStream(id);
     this.player = flv.createPlayer({
       type: 'flv',
       url: `http://localhost:8000/live/${id}.flv`
